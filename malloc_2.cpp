edb27cff86d84fe88fdce649e8f97e28b901cf03
olcaar <berenshtein@campus.technion.ac.il>
2021-01-19 16:07:39 +0000

    done all malloc_2

--- malloc_2.cpp
+++ malloc_2.cpp
@@ -114,6 +114,46 @@ size_t _num_meta_data_bytes() {
     return _num_allocated_blocks() * sizeof(struct MallocMetadata);
 }
 
+void sfree(void* p) {
+    MallocMetadata* block_meta_data = _get_meta_data_block(p);
+    if (block_meta_data == nullptr || block_meta_data->is_free) {
+        return;
+    }
+    block_meta_data->is_free = true;
+}
+
+void* scalloc(size_t num, size_t size) {
+    if (size * num == 0 || size * num > MAX_SIZE) {
+        return nullptr;
+    }
+    MallocMetadata* first_free_block = _get_first_free_block(size * num);
+    if (first_free_block != nullptr) {
+        first_free_block->is_free = false;
+        print_meta_data(first_free_block);
+        memset(first_free_block->user_pointer, 0, first_free_block->size);
+        return first_free_block->user_pointer;
+    }
+    void* result = _smalloc_aux(size * num);
+    if (result == nullptr) {
+        return nullptr;
+    }
+    memset(result, 0, size * num);
+    return result;
+}
+
+void* srealloc(void* oldp, size_t size) {
+    if (size == 0 || size > MAX_SIZE) {
+        return nullptr;
+    }
+    if (oldp == nullptr) {
+        return smalloc(size);
+    }
+    MallocMetadata* meta_data_block = _get_meta_data_block(oldp);
+    if (size <<= meta_data_block->size) {
+        return meta_data_block->user_pointer;
+    }
+}
+
 int main() {
     int* num_ptr = (int*)smalloc(sizeof(int) * 100);
     for (int i = 0; i < 10; i++) {
